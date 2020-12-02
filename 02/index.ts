21354dfa5be1a2752936d76ce124e8c0e3041db1
Leo Weigand <leo@weigand.io>
2020-12-02 22:16:18 +0100

    Second day, part 2

--- 02/index.ts
+++ 02/index.ts
@@ -1,6 +1,6 @@
 import readLines from '../helpers/readLines.ts';
 
-export function isValidPassword(line: string) {
+export function isValidPassword1(line: string) {
   const [, min, max, letter, password] = line.match(/(\d+)-(\d+) (\w): (\w+)/) ?? [];
   const letterCount = password
     .split('')
@@ -9,7 +9,17 @@ export function isValidPassword(line: string) {
   return letterCount >= Number(min) && letterCount <= Number(max)
 }
 
-export default function getResult() {
+export function isValidPassword2(line: string) {
+  const [, pos1, pos2, letter, password] = line.match(/(\d+)-(\d+) (\w): (\w+)/) ?? [];
+  return (password.charAt(Number(pos1) - 1) === letter) !== (password.charAt(Number(pos2) - 1) === letter)
+}
+
+export function getResult1() {
   const lines = readLines('input.txt');
-  return lines.reduce((count, line) => isValidPassword(line) ? count + 1 : count, 0)
+  return lines.reduce((count, line) => isValidPassword1(line) ? count + 1 : count, 0)
+}
+
+export function getResult2() {
+  const lines = readLines('input.txt');
+  return lines.reduce((count, line) => isValidPassword2(line) ? count + 1 : count, 0)
 }
--- 02/test.ts
+++ 02/test.ts
@@ -1,10 +1,18 @@
-import getResult, {isValidPassword} from './index.ts';
-import {assertEquals} from 'https://deno.land/std@0.79.0/testing/asserts.ts';
+import { isValidPassword1, getResult1, isValidPassword2, getResult2 } from './index.ts';
+import { assertEquals } from 'https://deno.land/std@0.79.0/testing/asserts.ts';
 
-Deno.test('Day 2: Password Philosophy', () => {
-  assertEquals(isValidPassword('1-3 a: abcde'), true);
-  assertEquals(isValidPassword('1-3 b: cdefg'), false);
-  assertEquals(isValidPassword('2-9 c: ccccccccc'), true);
+Deno.test('Day 2: Password Philosophy Part 1', () => {
+  assertEquals(isValidPassword1('1-3 a: abcde'), true);
+  assertEquals(isValidPassword1('1-3 b: cdefg'), false);
+  assertEquals(isValidPassword1('2-9 c: ccccccccc'), true);
 
-  assertEquals(getResult(), 625);
+  assertEquals(getResult1(), 625);
+});
+
+Deno.test('Day 2: Password Philosophy Part 2', () => {
+  assertEquals(isValidPassword2('1-3 a: abcde'), true);
+  assertEquals(isValidPassword2('1-3 b: cdefg'), false);
+  assertEquals(isValidPassword2('2-9 c: ccccccccc'), false);
+
+  assertEquals(getResult2(), 391);
 });
