253241b916910fbf2007a1f52170227d3a639ed0
Leo Weigand <leo@weigand.io>
2020-12-04 18:23:50 +0100

    Add day 4, part 1

---
+++ 04/index.ts
@@ -0,0 +1,41 @@
+import * as path from 'https://deno.land/std/path/mod.ts';
+
+export function parseDocuments(batch: string) {
+  const rawDocuments = batch.split('\n\n');
+  const documents = rawDocuments
+    .map(doc => {
+      const fields = Array.from(doc.matchAll(/(\w{3}):(.+?)\s?/g), matches =>
+        matches.slice(1),
+      );
+
+      return Object.fromEntries(fields);
+    })
+    .filter(Boolean);
+
+  return documents;
+}
+
+export function isValidDocument(document: object) {
+  const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+
+  for (let i = 0; i < requiredFields.length; i++) {
+    if (!(requiredFields[i] in document)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function part1() {
+  const input = Deno.readTextFileSync(
+    path.resolve(import.meta.url.slice(5), '../input.txt'),
+  );
+  const documents = parseDocuments(input);
+
+  return documents.reduce(
+    (validCount, document) =>
+      isValidDocument(document) ? validCount + 1 : validCount,
+    0,
+  );
+}
