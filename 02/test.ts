e931299cf14ef8bd51062e5ad80a47db6fb1baff
Leo Weigand <leo@weigand.io>
2020-12-05 18:41:15 +0100

    Remove meaningless test

--- 02/test.ts
+++ 02/test.ts
@@ -5,14 +5,10 @@ Deno.test('Day 2: Password Philosophy Part 1', () => {
   assertEquals(isValidPassword1('1-3 a: abcde'), true);
   assertEquals(isValidPassword1('1-3 b: cdefg'), false);
   assertEquals(isValidPassword1('2-9 c: ccccccccc'), true);
-
-  assertEquals(part1(), 625);
 });
 
 Deno.test('Day 2: Password Philosophy Part 2', () => {
   assertEquals(isValidPassword2('1-3 a: abcde'), true);
   assertEquals(isValidPassword2('1-3 b: cdefg'), false);
   assertEquals(isValidPassword2('2-9 c: ccccccccc'), false);
-
-  assertEquals(part2(), 391);
 });
