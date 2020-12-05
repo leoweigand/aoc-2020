a5bcd88230730388d73e9491b6aa121007b395bc
Leo Weigand <leo@weigand.io>
2020-12-05 18:39:18 +0100

    Day 5 part 1

---
+++ 05/index.ts
@@ -0,0 +1,33 @@
+const calculatePosition = (positions: boolean[], length: number) =>
+  positions.reduce(
+    (rows, next) =>
+      rows.slice(
+        next ? 0 : rows.length / 2,
+        next ? rows.length / 2 : rows.length,
+      ),
+    Array.from({length: length}, (_, i) => i),
+  )[0];
+
+const calculateSeat = (seat: string) => {
+  const rows = seat
+    .slice(0, 7)
+    .split('')
+    .map(row => row === 'F');
+  const columns = seat
+    .slice(7)
+    .split('')
+    .map(col => col === 'L');
+
+  return calculatePosition(rows, 128) * 8 + calculatePosition(columns, 8);
+};
+
+export function part1(input: string) {
+  const boardingPasses = input.split('\n');
+
+  let highestSeat = 0;
+  for (let boardingPass of boardingPasses) {
+    highestSeat = Math.max(highestSeat, calculateSeat(boardingPass));
+  }
+
+  return highestSeat;
+}
